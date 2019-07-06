59f3295848a5ad1e65e407f3128c8f9463b1b1a6
Peter Suhm <peter@suhm.dk>
2019-07-06 21:22:53 +0200

    initial commit

---
+++ wp-content/plugins/awesome-wordpress-plugin/plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Awesome WordPress Plugin
+ * Author: WP Pusher
+ * Version: 22.0.0
+ */
+
+add_filter( "wp_loaded", "checkForGreeting" );
+
+function checkForGreeting()
+{
+    if (isset($_GET['hello'])) {
+
+        echo "Hello " . $_GET['hello'] . "!";
+        die();
+
+    }
+}
